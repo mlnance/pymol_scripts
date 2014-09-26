d9c5f9792e5c7f0558ffb8ec1193128769d2d7ec
mlnance <mlnance@ucdavis.edu>
2014-09-26 15:49:08 -0700

    started writing a unit test for the converter, still broken, the test doesnt make it to completion even though it should

--- pymol_pdb_to_mol_converter.py
+++ pymol_pdb_to_mol_converter.py
@@ -14,16 +14,33 @@ from pymol import cmd
 
 def convert_that_file_mol(file):
     'Takes a chemical structure file and uses pymol to save it as a mol file'
-    print "Reading %s to convert to mol format \n" %file
-    path = os.path.abspath(file)
-    name = path.split('/')[-1].split('.')[0]
+    try:
+        print "Reading %s to convert to mol format \n" %file
+        path = os.path.abspath(file)
+        name = path.split('/')[-1].split('.')[0]
 
-    cmd.load(file)
-    cmd.save("%s.mol" %name,name)
-    cmd.do("reinitialize")
+        cmd.load(file)
+        cmd.save("%s.mol" %name,name)
+        cmd.do("reinitialize")
+    except:
+        print "I can't open pymol or convert the file"
+
+def compare_results(good,new):
+    import filecmp
+    filecmp.cmp(good,new)
+
+def test():
+    test_file="/home/mlance/pymol_scripts/tests/1a99_main_chain.pdb.ligand"
+    good_file="/home/mlnance/pymol_scripts/tests/output.test"
+    try: 
+        convert_that_file_mol(test_file)
+        compare_results(good_file,"1a99_main_chain.mol")    
+    except:
+        print "I tried to work correctly, but I don't... DONT USE ME!"
 
 # main
 def main():
+#    test()
     file=sys.argv[1]
     print "Here is my file name %s \n" %file
     convert_that_file_mol(file)
